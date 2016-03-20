593cacdae4409f0bbab297d8973add8d7c374747
Jan Vlcek <vlki@vlki.cz>
2016-03-20 12:01:53 +0100

    Updated the brew install script to packages I really use

--- brew.sh
+++ brew.sh
@@ -49,7 +49,6 @@ brew install vim --override-system-vi
 brew install homebrew/dupes/grep
 brew install homebrew/dupes/openssh
 brew install homebrew/dupes/screen
-brew install homebrew/php/php56 --with-gmp
 
 # Install font tools.
 brew tap bramstein/webfonttools
@@ -57,52 +56,35 @@ brew install sfnt2woff
 brew install sfnt2woff-zopfli
 brew install woff2
 
-# Install some CTF tools; see https://github.com/ctfs/write-ups.
-brew install aircrack-ng
-brew install bfg
-brew install binutils
-brew install binwalk
-brew install cifer
-brew install dex2jar
-brew install dns2tcp
-brew install fcrackzip
-brew install foremost
-brew install hashpump
-brew install hydra
-brew install john
-brew install knock
-brew install netpbm
-brew install nmap
-brew install pngcheck
-brew install socat
-brew install sqlmap
-brew install tcpflow
-brew install tcpreplay
-brew install tcptrace
-brew install ucspi-tcp # `tcpserver` etc.
-brew install xpdf
-brew install xz
-
 # Install other useful binaries.
-brew install ack
-brew install dark-mode
-#brew install exiv2
 brew install git
-brew install git-lfs
-brew install imagemagick --with-webp
-brew install lua
-brew install lynx
-brew install p7zip
-brew install pigz
+
+# For toggling between light & dark mode of OS X
+brew install dark-mode
+
+# Monitors data going through pipe between unix commands
 brew install pv
+
+# Batch renaming tool
 brew install rename
-brew install rhino
-brew install speedtest_cli
-brew install ssh-copy-id
-brew install testssl
+
+# Opensource implementation of JavaScript written entirely in Java
+# brew install rhino
+
+# Command line interface for testing internet bandwidth using speedtest.net
+# brew install speedtest_cli
+
+# Installs public key to target server's authorized_keys
+# brew install ssh-copy-id
+
+# Tool for testing any server port for the SSL/TLS ciphers, etc.
+# brew install testssl
+
+# Recursive directory listing command
 brew install tree
-brew install webkit2png
-brew install zopfli
+
+# Commandline tool for doing screenshots of webpages
+# brew install webkit2png
 
 # Remove outdated versions from the cellar.
 brew cleanup
